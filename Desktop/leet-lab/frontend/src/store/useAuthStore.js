4f69aedda8c869e9b3ae516b30652e3831ced52d
Shubham Singh <shubham@techtoabroad.com>
2025-05-28 22:23:34 +0530

    signup and login method is added

--- Desktop/leet-lab/frontend/src/store/useAuthStore.js
+++ Desktop/leet-lab/frontend/src/store/useAuthStore.js
@@ -24,3 +24,36 @@ checkAuth: async () => {
     set({ isCheckingAuth: false });
   }
 };
+
+
+signup: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post(REGISTER_URL, data);
+  
+      set({ authUser: res.data.user });
+  
+      toast.success(res.data.message);
+    } catch (error) {
+      console.log("error in signing up");
+      toast.error("Error in signing up");
+    } finally {
+      set({ isSigningUp: false });
+    }
+  };
+  
+  login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await axios.post(LOGIN_URL, data);
+  
+      set({ authUser: res.data.user });
+  
+      toast.success(res.data.user);
+    } catch (error) {
+      console.log("error in loging in");
+      toast.error("Error in login");
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  };
