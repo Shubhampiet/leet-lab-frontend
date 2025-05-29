099ec299ac526b4a31772d07d608b9b6d747c5cb
Shubham Singh <shubham@techtoabroad.com>
2025-05-29 07:03:49 +0530

    Sign up page added

--- Desktop/leet-lab/frontend/src/constant/urls.js
+++ Desktop/leet-lab/frontend/src/constant/urls.js
@@ -1,5 +1,16 @@
-//Authentication urls
-export const CHECK_URL = "/auth/check";
-export const REGISTER_URL = "/auth/register";
-export const LOGIN_URL = "/auth/login";
-export const LOGOUT_URL = "/auth/logout";
+//Api urls
+
+//Authentication
+export const AUTH = "/auth";
+export const CHECK_URL = AUTH + "/check";
+export const REGISTER_URL = AUTH + "/register";
+export const LOGIN_URL = AUTH + "/login";
+export const LOGOUT_URL = AUTH + "/logout";
+
+//Navigation urls
+
+//Authentication
+
+export const HOME = "/"
+export const LOGIN = "/login"
+export const SIGNUP = "/signup"
