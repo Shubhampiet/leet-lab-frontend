21959d13b2f5aff7b100fcd6dc9ecf6d82b67b6a
Shubham Singh <shubham@techtoabroad.com>
2025-05-29 09:04:46 +0530

    conditions for page rendering added

--- Desktop/leet-lab/frontend/src/App.jsx
+++ Desktop/leet-lab/frontend/src/App.jsx
@@ -1,25 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
+import { useAuthStore } from "./store/useAuthStore";
+import { Home, Loader } from "lucide-react";
+import Layout from "./layout/Layout";
 
 const App = () => {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col items-center justify-start">
+      <Toaster />
       <Routes>
-        <Route 
-        path="/"
-        element={<HomePage />}
+      <Route path="/" element={<Layout />}>
+          <Route
+            index
+            element={authUser ? <HomePage /> : <Navigate to={"/login"} />}
+          />
+        </Route>
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
         />
-         <Route 
-        path="/login"
-        element={<LoginPage />}
-        />
-         <Route 
-        path="/signup"
-        element={<SignUpPage />}
-        />
+        <Route path="/signup" element={<SignUpPage />} />
+
       </Routes>
     </div>
   );
