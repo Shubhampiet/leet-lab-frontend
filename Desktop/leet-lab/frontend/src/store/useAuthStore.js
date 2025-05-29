9eaec7b32c38165eb18f52679f116e911b50fb06
Shubham Singh <shubham@techtoabroad.com>
2025-05-29 07:20:31 +0530

    logout method added

--- Desktop/leet-lab/frontend/src/store/useAuthStore.js
+++ Desktop/leet-lab/frontend/src/store/useAuthStore.js
@@ -1,38 +1,41 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
-import { CHECK_URL, LOGIN_URL, LOGOUT_URL, REGISTER_URL } from "../constant/urls";
+import {
+  CHECK_URL,
+  LOGIN_URL,
+  LOGOUT_URL,
+  REGISTER_URL,
+} from "../constant/urls";
 
 export const useAuthStore = create((set) => ({
   authUser: null,
   isSigningUp: false,
   isLoggingUp: false,
   isCheckingAuth: false,
-}));
 
-checkAuth: async () => {
-  set({ isCheckingAuth: true });
-  try {
-    const res = await axiosInstance.get(CHECK_URL);
-    console.log("Auth res", res.data);
+  checkAuth: async () => {
+    set({ isCheckingAuth: true });
+    try {
+      const res = await axiosInstance.get(CHECK_URL);
+      console.log("Auth res", res.data);
 
-    set({ authUser: res.data.user });
-  } catch (error) {
-    console.log("error in auth");
-    set({ authUser: null });
-  } finally {
-    set({ isCheckingAuth: false });
-  }
-};
+      set({ authUser: res.data.user });
+    } catch (error) {
+      console.log("error in auth");
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
 
-
-signup: async (data) => {
+  signup: async (data) => {
     set({ isSigningUp: true });
     try {
       const res = await axiosInstance.post(REGISTER_URL, data);
-  
+
       set({ authUser: res.data.user });
-  
+
       toast.success(res.data.message);
     } catch (error) {
       console.log("error in signing up");
@@ -40,15 +43,15 @@ signup: async (data) => {
     } finally {
       set({ isSigningUp: false });
     }
-  };
-  
+  },
+
   login: async (data) => {
     set({ isLoggingIn: true });
     try {
       const res = await axios.post(LOGIN_URL, data);
-  
+
       set({ authUser: res.data.user });
-  
+
       toast.success(res.data.user);
     } catch (error) {
       console.log("error in loging in");
@@ -56,4 +59,16 @@ signup: async (data) => {
     } finally {
       set({ isLoggingIn: false });
     }
-  };
+  },
+
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+
+      toast.success("Logout successful");
+    } catch (error) {
+      toast.error("Error logging out");
+    }
+  },
+}));
